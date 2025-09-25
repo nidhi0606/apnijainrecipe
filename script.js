f81d8944e74ff756b09115e73c14fe90ccd2bff0
nidhi0606 <75186814+nidhi0606@users.noreply.github.com>
2025-09-25 13:51:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
-// Function to show selected recipe tab
+// Show selected recipe
 function openRecipe(evt, recipeName) {
   var i, tabcontent, tablinks;
 
-  // Hide all tabs
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
-  // Remove active style from buttons
   tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].style.backgroundColor = "";
   }
 
-  // Show current tab and highlight button
   document.getElementById(recipeName).style.display = "block";
   evt.currentTarget.style.backgroundColor = "#ff4500";
 }
 
-// Show the first recipe by default
+// Show first recipe by default
 document.addEventListener("DOMContentLoaded", function() {
   document.querySelector(".tablink").click();
 });
-
